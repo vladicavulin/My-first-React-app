729f4134c90efa458089dc301fd23d5e3ce3a473
vladica <vladicavulin@gmail.com>
2020-03-05 10:51:58 +0100

    Working on the form section.

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,28 +1,52 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 class MemeGenerator extends Component {
     constructor() {
         super()
-        this.state ={
-            character:""
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
         }
     }
-    
+
+
+
     componentDidMount() {
-        fetch("https://swapi.co/api/people/1/")
-        .then(response => response.json())
-        .then(data => {
-            this.setState({
-                character:data
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(data => {
+                const { memes } = data.data
+                this.setState({
+                    allMemeImgs: memes
+                })
             })
-        })
     }
+
     render() {
-    return (
-        <div>{this.state.character.name}</div>
-        
-    )
-}
+        return (
+            <div>
+                <form className="meme-form">
+                    <input type="text"
+                        name="topText"
+                        value={this.state.topText}
+                        placeholder="Top text"
+                    />
+                    <br />
+                    <input type="text"
+                        name="bottomText"
+                        value={this.state.bottomText}
+                        placeholder="Bottom text"
+                    />
+                    <br />
+
+                    <button>Generate</button>
+                </form>
+            </div>
+
+        )
+    }
 }
 
 export default MemeGenerator
