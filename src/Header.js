3dc800a8c950285d2ce4358c669d27f1b95143b0
vladica <vladicavulin@gmail.com>
2020-03-05 10:33:25 +0100

    A simple Header section is done.

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Header() {
     return (
-        <div>This is the header section </div>
+        <header>
+            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" />
+        <p>Meme Generator</p> 
+        </header>
     )
 }
 
